bffedd8fe43f78c2382ecc3a289f2851d6819822
dgrant6 <dgran013@plattsburgh.edu>
2023-01-12 00:29:23 -0500

    cleaner

--- encyclo/pitches/urls.py
+++ encyclo/pitches/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<pitch>", views.pitch_select),
+    path("", views.index),
+    path("<int:pitch>", views.pitch_by_num),
+    path("<str:pitch>", views.pitch_select,name='pitch_name'),
 ]
--- encyclo/pitches/views.py
+++ encyclo/pitches/views.py
@@ -1,21 +1,45 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+from django.template.loader import render_to_string
 
 pitch_selections = {
-    "sinker" : "sinker",
-    "curveball": "curveball",
-    "slider": "slider",
-    "changeup": "changeup",
-    "cutter": "cutter",
-    "splitter": "splitter",
-    "screwball": "screwball",
-    "knuckleball": "knuckleball",
+    "sinker" : "Sinker",
+    "curveball": "Curveball",
+    "slider": "Slider",
+    "changeup": "Changeup",
+    "cutter": "Cutter",
+    "splitter": "Splitter",
+    "screwball": "Screwball",
+    "knuckleball": "Knuckleball",
 }
 # Create your views here.
+def index(request):
+    items = ""
+    pitchs = list(pitch_selections.keys())
+    for pitch in pitchs:
+        capital = pitch.capitalize()
+        pitch_path = reverse('pitch_name', args=[pitch])
+        items += f"<li><a href=\"{pitch_path}\">{capital}</a></li>"
+    repsonse_data = f"<ul>{items}</ul>"
+    return HttpResponse(repsonse_data)
+
+def pitch_by_num(request, pitch):
+    pitchs = list(pitch_selections.keys())
+    if pitch > len(pitchs):
+        return HttpResponseNotFound("Invalid Pitch")
+    redirect_pitch = pitchs[pitch-1]
+    reditect_path = reverse('pitch_name', args=[redirect_pitch])
+    return HttpResponseRedirect(reditect_path)
+
 def pitch_select(request,pitch):
     try:
         temp = pitch_selections[pitch]
-        return HttpResponse(temp)
+        return render(request,'pitches/thepitch.html',{
+            "text": temp,
+            #"pitch_type" : pitch.capitalize()
+        })
+
     except:
         return HttpResponseNotFound("Pitch not here")
 
